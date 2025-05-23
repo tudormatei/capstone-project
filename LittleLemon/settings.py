e40ec3a2107b3d3aa45c13fbc3557040260f008d
tudormatei <tudormatei010@gmail.com>
2025-05-23 17:35:53 +0200

    djoser user authentication

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -38,7 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant',
+    'rest_framework',
+    'djoser'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
+
+DJOSER = {
+    "USER_ID_FIELD": "username",
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+from rest_framework import viewsets
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
